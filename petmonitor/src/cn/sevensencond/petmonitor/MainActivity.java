2c8db5f9e8a65f30c08487babde57a55b495c6f2
MichaelBai <bailu1234@gmail.com>
2013-12-15 16:50:04 +0800

    - transfer tab to spaces in MainActivity.java

--- petmonitor/src/cn/sevensencond/petmonitor/MainActivity.java
+++ petmonitor/src/cn/sevensencond/petmonitor/MainActivity.java
@@ -12,55 +12,53 @@ import android.widget.TextView;
 
 public class MainActivity extends Activity {
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.welcome);
-		
-		final petserver server = new petserver();
-		server.check_login(new serverHandle(){
-			@Override
-			public void onSuccess(String result)
-			{
-				petserver.userinfo ui = server.ui;
-				Log.v("a", ui.userMail);
-				setContentView(R.layout.activity_main);
-				
-				server.getuserinfo(new serverHandle(){
-					@Override
-					public void onSuccess(String result)
-					{
-						Log.v("server","getuserinfo ok");
-					}
-					@Override
-					public void onFailed(String result)
-					{
-						
-					}
-				});
-				
-			}
-			@Override
-			public void onFailed(String result)
-			{
-				
-			}
-		});
-	}
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.welcome);
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
-	}
+        final petserver server = new petserver();
+        server.check_login(new serverHandle() {
+            @Override
+            public void onSuccess(String result) {
+                petserver.userinfo ui = server.ui;
+                Log.v("a", ui.userMail);
+                setContentView(R.layout.activity_main);
 
-	public void sendMessage(View view) {
+                server.getuserinfo(new serverHandle() {
+                    @Override
+                    public void onSuccess(String result) {
+                        Log.v("server", "getuserinfo ok");
+                    }
+
+                    @Override
+                    public void onFailed(String result) {
+
+                    }
+                });
+
+            }
+
+            @Override
+            public void onFailed(String result) {
+
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    public void sendMessage(View view) {
         Intent intent = new Intent(this, MapActivity.class);
         startActivity(intent);
     }
-	
-	public void callOverview(View view) {
+
+    public void callOverview(View view) {
         Intent intent = new Intent(this, OverviewActivity.class);
         startActivity(intent);
     }
