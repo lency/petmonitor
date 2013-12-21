38df01d05bc6e61e42e2e4b79607a46afeede7c7
MichaelBai <bailu1234@gmail.com>
2013-12-21 22:24:42 +0800

    - fix overview.xml layout; add arrowUp and arrowDown; add bottom nav button

--- petmonitor/src/cn/sevensencond/petmonitor/OverviewActivity.java
+++ petmonitor/src/cn/sevensencond/petmonitor/OverviewActivity.java
@@ -2,55 +2,124 @@ package cn.sevensencond.petmonitor;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AbsListView;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 import cn.sevensencond.petmonitor.ScrollLayout.OnScreenChangeListener;
 import cn.sevensencond.petmonitor.ScrollLayout.OnScreenChangeListenerDataLoad;
 
 public class OverviewActivity extends Activity {
 
-    ListView list = null;
-    String[] web = { "Google Plus", "Twitter", "Windows", "Bing", "Sun", "Oracle" };
+    ListView devicesListView = null;
+    String[] web = { "Google Plus", "Twitter", "Windows", "Bing", "Sun",
+            "Oracle" };
     Integer[] imageId = { R.drawable.smallheader, R.drawable.smallheader,
-            R.drawable.smallheader, R.drawable.smallheader, R.drawable.smallheader, R.drawable.smallheader};
+            R.drawable.smallheader, R.drawable.smallheader,
+            R.drawable.smallheader, R.drawable.smallheader };
+
+    private View mDevicesNoneTips = null;
+    private View mDevicesViewTips = null;
+    private View mDevicesUpArrow = null;
+    private View mDevicesDownArrow = null;
+    private int mDevicesCount = 0;
+    
+    private AbsListView.OnScrollListener devicesListViewScrollListener = new AbsListView.OnScrollListener() {
+        public void onScroll(AbsListView view, int firstVisibleItem, int visibleItemCount, int totalItemCount) {
+            Log.d("ListView Scroll", "firstVisibleItem: "+firstVisibleItem);
+            Log.d("ListView Scroll", "visibleItemCount: "+visibleItemCount);
+            Log.d("ListView Scroll", "totalItemCount: "+totalItemCount);
+            
+            if (firstVisibleItem > 0) {
+                mDevicesUpArrow.setVisibility(View.VISIBLE);
+            } else {
+                mDevicesUpArrow.setVisibility(View.INVISIBLE);
+            }
+            if (firstVisibleItem + visibleItemCount < totalItemCount ) {
+                mDevicesDownArrow.setVisibility(View.VISIBLE);
+            } else {
+                mDevicesDownArrow.setVisibility(View.INVISIBLE);
+            }
+        }
+
+        public void onScrollStateChanged(AbsListView view, int scrollState) {
+            Log.d("ListView Scroll", "Scroll state changed");
+            
+        }
+    };
+    
+    private ImageView mImgNavDev = null;
+    private ImageView mImgNavFriend = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.overview);
+        
+        mImgNavDev = (ImageView)findViewById(R.id.imgnav_dev);
+        mImgNavFriend = (ImageView)findViewById(R.id.imgnav_friend);
 
-        ScrollLayout scrollLayout = (ScrollLayout)findViewById(R.id.main_viewflipper);
+        ScrollLayout scrollLayout = (ScrollLayout) findViewById(R.id.main_viewflipper);
         scrollLayout.setOnScreenChangeListener(new OnScreenChangeListener() {
-            
             @Override
             public void onScreenChange(int currentIndex) {
                 // TODO Auto-generated method stub
+                if (currentIndex == 0) {
+                    mImgNavDev.setImageResource(R.drawable.nav1);
+                    mImgNavFriend.setImageResource(R.drawable.nav2);
+                } else {
+                    mImgNavDev.setImageResource(R.drawable.nav2);
+                    mImgNavFriend.setImageResource(R.drawable.nav1);
+                }
                 
             }
         });
         scrollLayout.setOnScreenChangeListenerDataLoad(new OnScreenChangeListenerDataLoad() {
             @Override
             public void onScreenChange(int currentIndex) {
-                
+
             }
         });
-//        CustomList adapter = new CustomList(OverviewActivity.this, web, imageId);
-//        list = (ListView) findViewById(R.id.main_listview_devices);
-//        list.setAdapter(adapter);
-//        list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-//            @Override
-//            public void onItemClick(AdapterView<?> parent, View view,
-//                    int position, long id) {
-//                Toast.makeText(OverviewActivity.this,
-//                        "You Clicked at " + web[+position], Toast.LENGTH_SHORT)
-//                        .show();
-//            }
-//        });
+
+        CustomList adapter = new CustomList(OverviewActivity.this, web, imageId);
+        devicesListView = (ListView) findViewById(R.id.main_listview_devices);
+        devicesListView.setAdapter(adapter);
+        devicesListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(OverviewActivity.this,
+                        "You Clicked at " + web[+position], Toast.LENGTH_SHORT)
+                        .show();
+            }
+        });
+        
+        mDevicesNoneTips = findViewById(R.id.devices_none_tips);
+        mDevicesViewTips = findViewById(R.id.devices_view_tips);
+        mDevicesUpArrow = findViewById(R.id.devices_view_uparrow);
+        mDevicesDownArrow = findViewById(R.id.devices_view_downarrow);
+        mDevicesNoneTips.setVisibility(View.GONE);
+        mDevicesViewTips.setVisibility(View.GONE);
+//        mDevicesUpArrow.setVisibility(View.GONE);
+//        mDevicesDownArrow.setVisibility(View.GONE);
+        
+        // init arrowUp and arrowDown
+        mDevicesCount = web.length;
+        if (devicesListView.getFirstVisiblePosition() > 0) {
+            mDevicesUpArrow.setVisibility(View.VISIBLE);
+        }
+        if (devicesListView.getLastVisiblePosition() < mDevicesCount - 1) {
+            mDevicesDownArrow.setVisibility(View.VISIBLE);
+        }
+        
+        devicesListView.setOnScrollListener(devicesListViewScrollListener);
     }
 
     // @Override
@@ -76,9 +145,11 @@ public class OverviewActivity extends Activity {
         public View getView(int position, View view, ViewGroup parent) {
             LayoutInflater inflater = context.getLayoutInflater();
             View rowView = inflater.inflate(R.layout.list_single, null, true);
-            TextView txtTitle = (TextView) rowView.findViewById(R.id.mylistitem_text_name);
+            TextView txtTitle = (TextView) rowView
+                    .findViewById(R.id.mylistitem_text_name);
             txtTitle.setText(web[position]);
-            ImageButton imageButton = (ImageButton) rowView.findViewById(R.id.mylistitem_button_call);
+            ImageButton imageButton = (ImageButton) rowView
+                    .findViewById(R.id.mylistitem_button_call);
             if (position == 3) {
                 imageButton.setImageResource(R.drawable.locator_phone_disable);
             }
